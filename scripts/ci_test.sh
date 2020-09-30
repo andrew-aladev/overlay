97651d0b807ad78107a0a64815b6a3a0a372bd96
Andrew Aladjev <aladjev.andrew@gmail.com>
2020-09-30 11:38:19 +0300

    improved not important uses functionality

--- scripts/ci_test.sh
+++ scripts/ci_test.sh
@@ -2,10 +2,11 @@
 set -e
 
 OVERLAY_NAME="andrew-aladev"
-IGNORE_USES=(
+NOT_IMPORTANT_USES=(
   "abi_*"
   "noman"
   "ruby_targets_*"
+  "static*"
   "test"
 )
 
@@ -16,33 +17,33 @@ for package in "${packages[@]}"; do
   # List available uses for package.
   mapfile -t uses < <(equery --quiet uses "$package" | sed "s/^[+-]//")
 
-  # Ignoring several uses.
-  filtered_uses=()
+  # Ignoring not important uses.
+  important_uses=()
 
   for use in "${uses[@]}"; do
-    is_use_valid=true
+    is_use_important=true
 
-    for IGNORE_USE in "${IGNORE_USES[@]}"; do
-      if [[ "$use" == $IGNORE_USE ]]; then
-        is_use_valid=false
+    for NOT_IMPORTANT_USE in "${NOT_IMPORTANT_USES[@]}"; do
+      if [[ "$use" == $NOT_IMPORTANT_USE ]]; then
+        is_use_important=false
         break
       fi
     done
 
-    if [ "$is_use_valid" = true ]; then
-      filtered_uses+=("$use")
+    if [ "$is_use_important" = true ]; then
+      important_uses+=("$use")
     fi
   done
 
   # Emerging package with all possible use combinations.
-  filtered_uses_length="${#filtered_uses[*]}"
-  combinations_length=$((1 << $filtered_uses_length))
+  important_uses_length="${#important_uses[*]}"
+  combinations_length=$((1 << $important_uses_length))
 
   for ((combination = 0; combination < $combinations_length; combination++)); do
     current_uses=()
 
-    for use_index in "${!filtered_uses[@]}"; do
-      use="${filtered_uses[$use_index]}"
+    for use_index in "${!important_uses[@]}"; do
+      use="${important_uses[$use_index]}"
       use_value=$((1 << ($use_index - 1)))
 
       if [[ $(($combination & $use_value)) -eq $use_value ]]; then
