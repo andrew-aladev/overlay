62c48db7a1398fa1e3b571c3fd432fb36a6a2a36
Andrew Aladjev <aladjev.andrew@gmail.com>
2021-05-20 00:56:34 +0300

    cosmetics

--- scripts/add_remotes.sh
+++ scripts/add_remotes.sh
@@ -4,6 +4,6 @@ set -e
 DIR=$(dirname "${BASH_SOURCE[0]}")
 cd "$DIR"
 
-git remote add github    "git@github.com:andrew-aladev/overlay.git"    || true
-git remote add bitbucket "git@bitbucket.org:andrew-aladev/overlay.git" || true
-git remote add gitlab    "git@gitlab.com:andrew-aladev/overlay.git"    || true
+git remote add github    "git@github.com:andrew-aladev/overlay.git"    || :
+git remote add bitbucket "git@bitbucket.org:andrew-aladev/overlay.git" || :
+git remote add gitlab    "git@gitlab.com:andrew-aladev/overlay.git"    || :
